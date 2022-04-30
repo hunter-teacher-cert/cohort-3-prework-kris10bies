e8d21ac866afaa2aebbcb27bdec455c523b278c2
kris10bies <104540936+kris10bies@users.noreply.github.com>
2022-04-30 17:07:43 +0000

    added GuessStarter

---
+++ pre08/Craps.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Craps{
+
+  
+  public static void main(String[] args){
+
+    roll();
+      newLine();
+    shoot();
+      newLine();
+    round(); 
+
+    
+  }
+ 
+}
