6154af878ebf5d34aef6caef3a3793e7be36224f
kris10bies <104540936+kris10bies@users.noreply.github.com>
2022-04-30 02:27:39 +0000

    added loop to pre05

---
+++ pre05/Loop.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Loop{
+public static void main(String [] args){
+
+
+  int i = 10;
+  int sum = 0;
+  int dif = 0;
+  sum = i + 1;
+  dif = i - 2;
+    
+  System.out.println(sum);
+  System.out.println(dif);
+  }
+}
